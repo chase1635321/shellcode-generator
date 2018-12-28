82d98189aa34c6ab6444258a0ab84ca39e6c749a
chase1635321 <chasekanipe@gmail.com>
2018-12-28 11:30:38 -0500

    Second commit

--- gen.py
+++ gen.py
@@ -7,12 +7,12 @@ print("Commands: compile, run, generate")
 while cmd != "exit":
     cmd = input()
     if cmd == "compile":
-        print("gcc exec.c -fno-stack-protector -z execstack -o exec.x")
-        os.system("gcc exec.c -fno-stack-protector -z execstack -o exec.x")
+        print("gcc src/exec.c -fno-stack-protector -z execstack -o src/exec.x")
+        os.system("gcc src/exec.c -fno-stack-protector -z execstack -o src/exec.x")
     if cmd == "run":
-        os.system("./exec.x")
+        os.system("./src/exec.x")
     if cmd == "clear":
         os.system("clear")
     if cmd == "generate":
-        os.system("gcc shellcode.c -o shellcode.x");
-        os.system("./generate_shellcode.sh")
+        os.system("gcc src/shellcode.c -o src/shellcode.x");
+        os.system("./src/generate_shellcode.sh")
